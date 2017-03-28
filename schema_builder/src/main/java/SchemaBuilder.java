268b7ae3438d6087874a5ae334d2afbe5148dd99
CaptainRandom <markj@ispot.tv>
2017-03-28 13:21:34 -0700

    Adding the ability to specify order in the schema

--- schema_builder/src/main/java/SchemaBuilder.java
+++ schema_builder/src/main/java/SchemaBuilder.java
@@ -5,6 +5,7 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.sql.Date;
 import java.sql.Timestamp;
@@ -21,6 +22,13 @@ public class SchemaBuilder {
         // TODO: Probably want to filter out protected/private fields that don't have any getters.
         List<StructField> sparkFieldList = FieldUtils.getAllFieldsList(type).stream()
                 .filter(field -> !Modifier.isStatic(field.getModifiers())) // don't get any static fields
+                .sorted((field1, field2) -> {
+                    SparkSchema schema1 = field1.getAnnotation(SparkSchema.class);
+                    SparkSchema schema2 = field2.getAnnotation(SparkSchema.class);
+                    int order1 = determineOrderNumer(field1, schema1);
+                    int order2 = determineOrderNumer(field2, schema2);
+                    return Integer.compare(order1,order2);
+                })
                 .map(field -> {
                     Optional<DataType> dataType = determineSimpleDataType(field.getType());
                     if(!dataType.isPresent()) {
@@ -33,11 +41,22 @@ public class SchemaBuilder {
                     return DataTypes.createStructField(field.getName(), dataType.get(), isNullable);
                 }).collect(Collectors.toList());
 
+
         return new StructType(sparkFieldList.toArray(new StructField[sparkFieldList.size()]));
     }
 
+    private static int determineOrderNumer(Field field, SparkSchema schema) {
+        if(schema == null) {
+            //TODO: log that the annotation is missing and return a default value
+            System.out.println(field.getName() + " has null annotation");
+            return Integer.MAX_VALUE;
+        } else {
+            return schema.order();
+        }
+    }
+
     // TODO: Figure out Array, Struct, and Map types.
-    public static Optional<DataType> determineSimpleDataType(Class type) {
+    private static Optional<DataType> determineSimpleDataType(Class type) {
         if(ClassUtils.isAssignable(type, String.class, true)) { return Optional.of(DataTypes.StringType); }
         else if(type.isArray() && ClassUtils.isAssignable(type.getComponentType(), Byte.class, true)) { return Optional.of(DataTypes.BinaryType); }
         else if(ClassUtils.isAssignable(type, Boolean.class, true)) { return Optional.of(DataTypes.BooleanType); }
--- schema_builder/src/main/java/SparkSchema.java
+++ schema_builder/src/main/java/SparkSchema.java
@@ -1,6 +1,11 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by markjackson on 3/18/17.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface SparkSchema {
     boolean isNullable() default true;
+    int order() default 0;
 }
